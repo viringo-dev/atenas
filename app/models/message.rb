f21c3faffe154bbbd8bb1e992130af5abb5126d4
Kevin <kevin@Kevins-MacBook-Pro.local>
2023-11-20 22:57:08 -0500

    Update callback

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,8 +7,9 @@ class Message < ApplicationRecord
   validates :content, presence: { allow_blank: false, message: :blank }
 
   ## CALLBACKS ##
-  after_create_commit -> { broadcast_append_later_to channel, partial: "channels/message", target: channel }
+  after_create_commit -> { broadcast_prepend_later_to channel.uuid, partial: "messages/message", target: channel.uuid }
 
   ## SCOPES ##
+  scope :ordered, ->(order = :asc) { order(created_at: order) }
   scope :paginated, ->(params={}) { page(params[:page]).per(params[:per_page]) }
 end
