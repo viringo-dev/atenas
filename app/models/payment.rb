8e30ab54f5dd7d3bfefa9899e19d55220c1a46d3
Kevin Sevinche <kevinsevinche@gmail.com>
2024-01-11 16:30:42 -0500

    Set attachment max size

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,7 @@
 class Payment < ApplicationRecord
+  ## CONSTANTS ##
+  MAX_ATTACHMENT_SIZE = 3
+
   ## ASSOCIATIONS ##
   belongs_to :bid
   has_one_attached :attachment
@@ -6,11 +9,21 @@ class Payment < ApplicationRecord
   ## VALIDATIONS ##
   validates :payer, presence: { allow_blank: false }
   validates :bid, uniqueness: true
+  validate :attachment_size
 
   ## ENUMS ##
   enum status: { pending: 0, validated: 1, rejected: 2 }
 
   def validate!
     Payments::ValidateService.new(payment: self).call
+  end
+
+  private
+
+  def attachment_size
+    if attachment.attached? && attachment.blob.byte_size > MAX_ATTACHMENT_SIZE.megabytes
+      attachment.purge
+      errors.add(:attachment, :too_big, max_size: MAX_ATTACHMENT_SIZE)
+    end
   end
 end
