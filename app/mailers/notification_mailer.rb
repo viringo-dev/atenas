d56030e5c9787e9f2c85f5ac02e2ab9701686980
Kevin Sevinche <kevinsevinche@gmail.com>
2024-01-13 16:44:13 -0500

    Add 'accepted bid' mail notification

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -9,4 +9,15 @@ class NotificationMailer < ApplicationMailer
     @task = @bid.task
     mail to: @task.user.email, subject: t("mailers.notification_mailer.new_bid.subject", app_name: "Atenas")
   end
+
+  def accepted_bid(notification_id)
+    @notification = Notification.find_by(id: notification_id)
+    return unless @notification
+
+    @bid = @notification.resource
+    return unless @bid
+
+    @task = @bid.task
+    mail to: @bid.user.email, subject: t("mailers.notification_mailer.accepted_bid.subject", app_name: "Atenas")
+  end
 end
