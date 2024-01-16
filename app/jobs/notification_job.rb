e4d4fc939fd4200810cebc41cac02b439080a029
Kevin Sevinche <kevinsevinche@gmail.com>
2024-01-15 20:03:26 -0500

    Create Notification for task owner when validate payment

--- app/jobs/notification_job.rb
+++ app/jobs/notification_job.rb
@@ -18,9 +18,14 @@ class NotificationJob < ApplicationJob
       bid = Bid.find_by(id: resource_id)
       if bid
         channel = bid.task.channel
-        notification = Notification.new(resource: bid, user: bid.user, notification_type: notification_type, path: messages_path(uuid: channel.uuid))
-        if notification.save
-          NotificationMailer.accepted_bid(notification.id).deliver_later
+        notification_for_bidder = Notification.new(resource: bid, user: bid.user, notification_type: notification_type, path: messages_path(uuid: channel.uuid))
+        if notification_for_bidder.save
+          NotificationMailer.accepted_bid(notification_for_bidder.id).deliver_later
+        end
+
+        notification_for_task_owner = Notification.new(resource: bid.task, user: bid.task.user, notification_type: Notification.notification_types[:payment_validated], path: messages_path(uuid: channel.uuid))
+        if notification_for_task_owner.save
+          NotificationMailer.payment_validated(notification_for_task_owner.id).deliver_later
         end
       end
     else
