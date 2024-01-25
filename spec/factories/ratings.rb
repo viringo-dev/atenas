b6477f7fdda4d458092cd67733fdf0470c8421e4
Kevin Sevinche <kevinsevinche@gmail.com>
2024-01-24 20:09:16 -0500

    Add ratings factory

---
+++ spec/factories/ratings.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :rating do
+    value { rand(1..5) }
+    comment { "This is a comment" }
+    rating_type { Rating.rating_types[:task_owner] }
+    association :bid, factory: :paid_bid
+
+    trait :task_assignee do
+      rating_type { Rating.rating_types[:task_assignee] }
+    end
+
+    after(:build) do |rating|
+      rating.task = rating.bid.task
+      rating.user = rating.task.user
+    end
+  end
+end
