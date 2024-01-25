f87d01e4e8aaf9c587f0c89cb14a27e39673a1ec
Kevin Sevinche <kevinsevinche@gmail.com>
2024-01-25 18:43:59 -0500

    Skip callback

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,5 +1,6 @@
 class ConfirmationsController < ApplicationController
   before_action :redirect_if_authenticated, only: [:create, :new]
+  skip_before_action :authenticate_user!, only: [:new, :create, :edit]
   
   def create
     @user = User.find_by(email: params[:user][:email].downcase)
