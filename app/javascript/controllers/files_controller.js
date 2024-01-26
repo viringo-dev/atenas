f7c0ee7e6592ca7b0779b016cee03bb042427a14
Kevin Sevinche <kevinsevinche@gmail.com>
2024-01-26 13:36:31 -0500

    Add method to check files size

--- app/javascript/controllers/files_controller.js
+++ app/javascript/controllers/files_controller.js
@@ -3,11 +3,24 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="files"
 export default class extends Controller {
   static targets = ["input"]
+  static values = { maxAttachmentSize: Number }
 
   checkFiles() {
     if (this.inputTarget.files.length > 5) {
       alert("Solo se permiten 5 archivos adjuntos.")
       this.inputTarget.value = ""
+    }
+    this.checkFilesSize()
+  }
+
+  checkFilesSize() {
+    let files = this.inputTarget.files
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].size > this.maxAttachmentSizeValue * 1024 * 1024) {
+        alert(`El límite de tamaño por archivo es de ${this.maxAttachmentSizeValue}MB.`)
+        this.inputTarget.value = ""
+        return
+      }
     }
   }
 }
