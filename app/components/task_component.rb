d66095b93cf49497f13c2a83ec910841847d618e
Kevin <kevinsevinche@gmail.com>
2024-02-14 17:14:43 -0500

    Add tests and refactor component-template

--- app/components/task_component.rb
+++ app/components/task_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TaskComponent < ApplicationComponent
-  attr_reader :task, :current_user
+  attr_reader :task, :current_user, :bid
   def initialize(task:, current_user: nil)
     @task = task
     @current_user = current_user
@@ -9,5 +9,14 @@ class TaskComponent < ApplicationComponent
 
   def current_page_is_task_path?
     @current_page_is_task_path ||= current_page?(task_path(task))
+  end
+
+  def current_user_can_edit_task?
+    Tasks::EditPolicy.new(user: current_user, task: task).allowed?
+  end
+
+  def bid
+    return unless current_user
+    @bid ||= task.bid_by_user(current_user)
   end
 end
--- spec/components/task_component_spec.rb
+++ spec/components/task_component_spec.rb
@@ -5,11 +5,24 @@ include ViewComponent::TestHelpers
 
 RSpec.describe TaskComponent, type: :component do
   let!(:task) { create(:task) }
+  let!(:current_user) { create(:confirmed_user) }
+
+  it "renders the task title, description, deadline, reward, and owner username" do
+    expect(
+      render_inline(described_class.new(task: task, current_user: current_user)).to_html
+    ).to include(
+      task.name,
+      task.description,
+      I18n.l(task.deadline, locale: :es, format: :long),
+      task.reward.to_s,
+      task.user.username
+    )
+  end
 
   context "when current page is not 'task/:id'" do
     it "renders the task title as a link" do
       expect(
-        render_inline(described_class.new(task: task)).css("a").to_html
+        render_inline(described_class.new(task: task, current_user: current_user)).css("a").to_html
       ).to include(
         task.name
       )
@@ -21,7 +34,7 @@ RSpec.describe TaskComponent, type: :component do
 
     it "doesn't render the task title as a link" do
       expect(
-        render_inline(described_class.new(task: task)).css("a").to_html
+        render_inline(described_class.new(task: task, current_user: current_user)).css("a").to_html
       ).to_not include(
         task.name
       )
@@ -31,7 +44,7 @@ RSpec.describe TaskComponent, type: :component do
   context "when task hasn't got files" do
     it "doesn't render the files dropdown" do
       expect(
-        render_inline(described_class.new(task: task)).css("button[data-dropdown-toggle='files-dropdown-#{task.id}'].hidden").to_html
+        render_inline(described_class.new(task: task, current_user: current_user)).css("button[data-dropdown-toggle='files-dropdown-#{task.id}'].hidden").to_html
       ).to include(
         I18n.t("activerecord.attributes.task.files")
       )
@@ -43,7 +56,7 @@ RSpec.describe TaskComponent, type: :component do
 
     it "renders the files dropdown" do
       expect(
-        render_inline(described_class.new(task: task)).css("button[data-dropdown-toggle='files-dropdown-#{task.id}'].hidden").to_html
+        render_inline(described_class.new(task: task, current_user: current_user)).css("button[data-dropdown-toggle='files-dropdown-#{task.id}'].hidden").to_html
       ).to_not include(
         I18n.t("activerecord.attributes.task.files")
       )
@@ -58,15 +71,95 @@ RSpec.describe TaskComponent, type: :component do
         I18n.t("activerecord.models.bid.other")
       )
     end
+
+    it "renders the edit and delete buttons" do
+      expect(
+        render_inline(described_class.new(task: task, current_user: task.user)).css("button").to_html
+      ).to include(
+        I18n.t("pages.common.edit")
+      )
+
+      expect(
+        render_inline(described_class.new(task: task, current_user: task.user)).css("button").to_html
+      ).to include(
+        I18n.t("pages.common.delete")
+      )
+    end
+
+    context "when task has a payment" do
+      let!(:task) { create(:assigned_task) }
+
+      it "doesn't render the edit and delete buttons" do
+        expect(
+          render_inline(described_class.new(task: task, current_user: task.user)).css("button").to_html
+        ).to_not include(
+          I18n.t("pages.common.edit")
+        )
+
+        expect(
+          render_inline(described_class.new(task: task, current_user: task.user)).css("button").to_html
+        ).to_not include(
+          I18n.t("pages.common.delete")
+        )
+      end
+    end
   end
 
   context "when current_user is not the task owner" do
     it "doesn't render the bids button" do
       expect(
-        render_inline(described_class.new(task: task)).css("a[href='/tasks/#{task.id}']").to_html
+        render_inline(described_class.new(task: task, current_user: current_user)).css("a[href='/tasks/#{task.id}']").to_html
       ).to_not include(
         I18n.t("activerecord.models.bid.other")
       )
+    end
+
+    it "doesn't render the edit and delete buttons" do
+      expect(
+        render_inline(described_class.new(task: task, current_user: current_user)).css("button").to_html
+      ).to_not include(
+        I18n.t("pages.common.edit")
+      )
+
+      expect(
+        render_inline(described_class.new(task: task, current_user: current_user)).css("button").to_html
+      ).to_not include(
+        I18n.t("pages.common.delete")
+      )
+    end
+
+    context "when current page is 'tasks/:id'" do
+      before { allow_any_instance_of(ActionView::Helpers::UrlHelper).to receive(:current_page?) { true } }
+
+      it "doesn't render the bid button" do
+        expect(
+          render_inline(described_class.new(task: task, current_user: current_user)).css("a[href='/tasks/#{task.id}']").to_html
+        ).to_not include(
+          I18n.t("pages.task.bid")
+        )
+      end
+    end
+
+    context "when current page is not 'tasks/:id'" do
+      it "renders the bid button" do
+        expect(
+          render_inline(described_class.new(task: task, current_user: current_user)).css("a[href='/tasks/#{task.id}']").to_html
+        ).to include(
+          I18n.t("pages.task.bid")
+        )
+      end
+
+      context "when current_user has already bided the task" do
+        let!(:bid) { create(:bid, task: task, user: current_user) }
+
+        it "renders a button (link) with the bid amount" do
+          expect(
+            render_inline(described_class.new(task: task, current_user: current_user)).css("a[href='/tasks/#{task.id}']").to_html
+          ).to include(
+            bid.amount.to_s
+          )
+        end
+      end 
     end
   end
 end
