bf0469671785d8347131850bddb692de84ba8d51
Kevin <kevinsevinche@gmail.com>
2024-02-13 19:17:49 -0500

    Add trait for task with file

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -6,6 +6,12 @@ FactoryBot.define do
     sequence(:deadline) { |n| Date.current + n.days }
     association :user, factory: :confirmed_user
 
+    trait :task_with_file do
+      after(:create) do |task|
+        task.files.attach(io: File.open(Rails.root.join("public", "logo.png")), filename: "logo.png", content_type: "image/png")
+      end
+    end
+
     factory :unpaid_task do
       status { Task.statuses[:unpaid] }
     end
