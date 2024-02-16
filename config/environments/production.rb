ed2359db5f3f5fab6ab7cbc71991c031743d5a3c
Kevin <kevinsevinche@gmail.com>
2024-02-16 17:53:04 -0500

    Setup Gmail SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,4 +96,16 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         "smtp.gmail.com",
+    port:            587,
+    domain:          "example.com",
+    user_name:       ENV["GMAIL_SMTP_USERNAME"],
+    password:        ENV["GMAIL_SMTP_PASSWORD"],
+    authentication:  "plain",
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5 }
 end
